9f1b65dc7a4e3c09ff248fc35d5ef5715cacf021
liisatormakangas <liisa.tormakangas@gmail.fi>
2023-11-19 15:07:38 +0200

    home view and notification added resolves #2

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,24 +1,49 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import { useParcelContext } from '../contexts/parcelContext';
 
-
-
+interface ParcelType {
+  id_parcel: number;
+};
+interface ParcelContextType {
+  incomingParcels: ParcelType[];
+};
 const NavbarHeader = () => {
+
+  const { incomingParcels } = useParcelContext() as ParcelContextType;
+
+  const numberOfParcels = () => {
+    switch (incomingParcels.length) {
+      case 0:
+        return ""
+      case 1:
+        return "You have 1 incoming parcel!"
+      default:
+        return `You have ${incomingParcels.length} incoming parcels!`
+    };
+  };
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand>App LOGO</Navbar.Brand>
+        {incomingParcels.length > 0 ? (
+          <div>
+            <Link className="navBarLink" to="/home">{numberOfParcels()}</Link>
+          </div>
+        ) : null}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav >
-            <Nav.Link href="/frontPage">Home</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
             <Nav.Link href="/signIn">Sign in</Nav.Link>
             <Nav.Link href="/Register">Register</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
-    </Navbar>
+    </Navbar >
   );
 }
 
