0e1fd8fbdabadc2f03924391d1a84671422155ec
Huyen Nguyen <c2ngth00@students.oamk.fi>
2023-12-05 08:28:42 +0200

    tested 3 views of send a new parcel

--- src/views/tests/SendNewParcel.test.tsx
+++ src/views/tests/SendNewParcel.test.tsx
@@ -1,40 +1,66 @@
-/* import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import SendNewParcel from "../SendNewParcel";
-// import child components
-import Sidebar from "../../components/sidebar";
-import ParcelSize from "../../components/ParcelSize";
-import ParcelSizeImage from "../../components/ParcelSizeImage";
-import SendParcelConfirm from "../../components/SendParcelConfirm";
-import ReceiverSenderDetails from "../../components/ReceiverSenderDetails";
-import { MemoryRouter } from "react-router"; */
-
-
+import SendNewParcel from "../sendNewParcel";
+import Signin from "../Signin";
+import { MemoryRouter } from "react-router"; 
 
 // Mock any external dependencies or functions
 
-/* test('renders Sidebar component', () => {
-    render(<MemoryRouter><Sidebar /></MemoryRouter>); */
-  
-    // Example: Check if a component or text is present
-    //expect(screen.getByText('My account')).toBeInTheDocument();
-  
-    // You can add more assertions based on your component's content and behavior
-  /* }); */
-  
-  /*test('clicking "Next" button advances to the next step', () => {
-    render(<SendNewParcel />);
-  
-    // Example: Clicking the "Next" button
-    fireEvent.click(screen.getByText('Next'));
-  
-    // Example: Check if the component or text related to the next step is present
-    expect(screen.getByText('Enter Receiver Details')).toBeInTheDocument();
-  
-    // Add more assertions based on your component's behavior
-  });*/
+test('renders SendNewParcel component', () => {
+    render(<MemoryRouter><SendNewParcel /></MemoryRouter>);
+    //Check if heading text first view is present
+    expect(screen.getByText('Send a New Parcel')).toBeInTheDocument();
+    expect(screen.getByText('Please fill in here package details. Note the maximum size!')).toBeInTheDocument();
+  }); 
 
+test('renders SendNewParcel second page', () => {
+    render(<MemoryRouter><SendNewParcel /></MemoryRouter>);
+ // find the Next button and click it
+    fireEvent.click(screen.getByText('Next'));
+    // check if the second page is rendered
+    expect(screen.getByText('Please fill in here receiver and sender details')).toBeInTheDocument();
+    // find the Back button and click it
+    fireEvent.click(screen.getByText('Back'));
+    // check if the first page is rendered
+    expect(screen.getByText('Please fill in here package details. Note the maximum size!')).toBeInTheDocument();
+  }); 
+
+  test('renders SendNewParcel 3rd page', () => {
+    render(<MemoryRouter><SendNewParcel /></MemoryRouter>);
+    // find the Next button and click it
+    fireEvent.click(screen.getByText('Next'));
+    // check if the second page is rendered
+    expect(screen.getByText('Please fill in here receiver and sender details')).toBeInTheDocument();
+ // find the Next button and click it
+    fireEvent.click(screen.getByText('Next'));
+    // check if the 3rd page is rendered
+    expect(screen.getByText('Please confirm your order!')).toBeInTheDocument();
+    // find the Back button and click it
+    fireEvent.click(screen.getByText('Back'));
+    // check if the first page is rendered
+    expect(screen.getByText('Please fill in here receiver and sender details')).toBeInTheDocument();
+  }); 
+
+  test('renders SendNewParcel 3rd page', () => {
+    render(<MemoryRouter><SendNewParcel /></MemoryRouter>);
+    // find the Next button and click it
+    fireEvent.click(screen.getByText('Next'));
+    // check if the second page is rendered
+    expect(screen.getByText('Please fill in here receiver and sender details')).toBeInTheDocument();
+ // find the Next button and click it
+    fireEvent.click(screen.getByText('Next'));
+    // check if the 3rd page is rendered
+    expect(screen.getByText('Please confirm your order!')).toBeInTheDocument();
+
+    test('validates form fields before confirming', async () => {
+      render(<SendNewParcel />);
+      const confirmButton = screen.getByText('Confirm');
+      fireEvent.click(confirmButton);
+      await waitFor(() => {
+        expect(screen.getByText('Please fill in all the fields.')).toBeInTheDocument();
+      });
+    });
   
-  
-  // Add more tests as needed to cover different scenarios 
+  }); 
+
